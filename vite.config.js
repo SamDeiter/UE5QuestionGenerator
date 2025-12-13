e7796566a81d5689213be4bcb07b3a24ca1e635f
SamDeiter <sam.deiter@epicgames.com>
2025-12-13 17:31:53 -0500

    chore: cleanup - organize root directory, move scripts and configs

--- vite.config.js
+++ vite.config.js
@@ -1,4 +1,4 @@
-import { defineConfig } from "vite";
+                                import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import { execSync } from "child_process";
 
