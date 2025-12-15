89e55aee56af56a28d7035af6da110ea823b02cd
SamDeiter <sam.deiter@epicgames.com>
2025-12-15 12:57:41 -0500

    chore: expose cleanupProductionDatabase on window for console access

--- src/main.jsx
+++ src/main.jsx
@@ -1,16 +1,19 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import ErrorBoundary from './components/ErrorBoundary.jsx'
-import ModalProvider from './contexts/ModalProvider.jsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import ErrorBoundary from "./components/ErrorBoundary.jsx";
+import ModalProvider from "./contexts/ModalProvider.jsx";
+import "./index.css";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-    <React.StrictMode>
-        <ErrorBoundary>
-            <ModalProvider>
-                <App />
-            </ModalProvider>
-        </ErrorBoundary>
-    </React.StrictMode>,
-)
+// Import cleanup utility to expose window.cleanupProductionDatabase for console access
+import "./utils/databaseCleanup.js";
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <ErrorBoundary>
+      <ModalProvider>
+        <App />
+      </ModalProvider>
+    </ErrorBoundary>
+  </React.StrictMode>
+);
