378136b217f592fa827854bc1c1ff2a9e79df568
SamDeiter <sam.deiter@epicgames.com>
2025-12-18 17:05:40 -0500

    refactor: extract utility functions

---
+++ functions/utils/grounding.js
@@ -0,0 +1,32 @@
+/**
+ * Utility: Extract Google Search grounding sources
+ * Extracted from index.js during modularization
+ */
+
+const admin = require("firebase-admin");
+
+function extractGroundingSources(responseData) {
+  const groundingMetadata = responseData.candidates?.[0]?.groundingMetadata;
+  const sources = [];
+
+  if (groundingMetadata?.groundingChunks) {
+    groundingMetadata.groundingChunks.forEach((chunk) => {
+      if (chunk.web?.uri && chunk.web?.title) {
+        const url = chunk.web.uri.toLowerCase();
+
+        // Only accept Epic Games documentation
+        if (url.includes("dev.epicgames.com/documentation")) {
+          sources.push({
+            url: chunk.web.uri,
+            title: chunk.web.title,
+          });
+        }
+      }
+    });
+  }
+
+  return sources;
+}
+
+
+module.exports = { grounding };
---
+++ functions/utils/isAdminUser.js
@@ -0,0 +1,13 @@
+/**
+ * Utility: Check if a user is an admin
+ * Extracted from index.js during modularization
+ */
+
+const admin = require("firebase-admin");
+
+/**
+ * Check if a user is an admin (from Firestore admins collection)
+ * @param {string} uid - User ID
+
+
+module.exports = { isAdminUser };
