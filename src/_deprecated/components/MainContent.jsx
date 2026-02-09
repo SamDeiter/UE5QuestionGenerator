47322d6fbc55ae24d34335dd256cbe8f6b5ccb5a
SamDeiter <sam.deiter@epicgames.com>
2026-02-09 13:01:15 -0500

    chore: delete _deprecated/ folder - 36 dead files permanently removed

--- src/_deprecated/components/MainContent.jsx
+++
@@ -1,9 +0,0 @@
-const MainContent = ({ children }) => {
-  return (
-    <main className="flex-1 flex flex-col min-w-0 bg-slate-950">
-      {children}
-    </main>
-  );
-};
-
-export default MainContent;
